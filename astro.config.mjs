2e0c0da1c964c11b9b1d09e51e58262ab02a14ef
rewelser <welser.re@gmail.com>
2026-05-08 21:17:44 -0400

    final pre-refactor tightening-up of booking page; fixing modal - pass 6 - making share functionality work - pass 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://blu-tattoo.vercel.app/",
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()]
