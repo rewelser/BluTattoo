61841cd9413106556839bca4fcedb20f0b9597f3
rewelser <welser.re@gmail.com>
2026-06-08 00:43:05 -0400

    finishing touches on homepage - pass 24 - taking a break to work on FAQ headings - posterity somewhat

--- src/domain/decor/defs.ts
+++ src/domain/decor/defs.ts
@@ -1,3 +1,14 @@
+import qm1 from "./assets/qm1.gif";
+import qm2 from "./assets/qm2.gif";
+import qm3 from "./assets/qm3.gif";
+import qm4 from "./assets/qm4.gif";
+import qm5 from "./assets/qm5.gif";
+import qm6 from "./assets/qm6.gif";
+import qm7 from "./assets/qm7.gif";
+import qm8 from "./assets/qm8.gif";
+import qm9 from "./assets/qm9.gif";
+import qm10 from "./assets/qm10.gif";
+
 export const frameTypes = [
     "roundframe1",
     "roundframe2",
@@ -10,3 +21,16 @@ export const frameTypes = [
 ] as const;
 
 export const frameType = [...frameTypes] as const;
+
+export const questionMarkImages = {
+    qm1,
+    qm2,
+    qm3,
+    qm4,
+    qm5,
+    qm6,
+    qm7,
+    qm8,
+    qm9,
+    qm10,
+} as const;
--- src/domain/decor/types.ts
+++ src/domain/decor/types.ts
@@ -1,6 +1,6 @@
 // import type {frameType} from "./defs.ts";
 
-import {frameTypes} from "./defs.ts";
+import {frameTypes, questionMarkImages} from "./defs.ts";
 import type {ImageMetadata} from "astro";
 
 // export type FrameType = typeof frameType[number];
@@ -9,3 +9,5 @@ export type FrameType = typeof frameTypes[number];
 export type ImageSourceOrMetadata =
     | string
     | ImageMetadata;
+
+export type QuestionMarkId = keyof typeof questionMarkImages;
