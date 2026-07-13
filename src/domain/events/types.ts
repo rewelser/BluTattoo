5535360e87596ded1a10101bd227cef397f6404c
rewelser <welser.re@gmail.com>
2026-07-13 04:03:39 -0400

    Temporal refactor - continuing effort; refactor of events/format.ts complete

--- src/domain/events/types.ts
+++ src/domain/events/types.ts
@@ -4,12 +4,6 @@ export type EventEntry = CollectionEntry<"events">;
 export type EventItem = EventEntry["data"] & { id: string; body?: string; occurrenceKey?: string };
 export type GuestItem = EventItem & { guestSpot: NonNullable<EventItem["guestSpot"]> & { shopClosed: false } };
 export type EventsByYearMonthDate = Record<string, Record<string, Record<string, EventItem[]>>>;
-export type DateParts = {
-    year: number;
-    month: number;
-    date: number;
-};
-
 
 /**
  * Event validation types
